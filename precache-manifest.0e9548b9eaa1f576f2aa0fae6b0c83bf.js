f283cb94c7b70afebdbed1880a41fd6c88b56db0
itouoti12 <white.sleipnir.jazz@gmail.com>
2020-10-11 03:57:18 +0900

    Updates

---
+++ precache-manifest.0e9548b9eaa1f576f2aa0fae6b0c83bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4aab596e1c61eed22b9fcff8479df2",
+    "url": "/order-management-react/index.html"
+  },
+  {
+    "revision": "b71ab5d0b9561e5a017f",
+    "url": "/order-management-react/static/js/2.1aa5efb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/order-management-react/static/js/2.1aa5efb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd9711065e467e3cee0",
+    "url": "/order-management-react/static/js/main.cc0d8b2e.chunk.js"
+  },
+  {
+    "revision": "cdda027455e49f7a7814",
+    "url": "/order-management-react/static/js/runtime-main.d09c82ec.js"
+  },
+  {
+    "revision": "3bbdc01df1c8c31b40edcc923653c83d",
+    "url": "/order-management-react/static/media/hotel_lounge.3bbdc01d.jpg"
+  }
+]);
